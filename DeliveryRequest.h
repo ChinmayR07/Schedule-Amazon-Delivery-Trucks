b44a638b88d1cc8025df4da39ccecad1e73d36ab
Vaibhav Tyagi <vaibhavtyagi@Vaibhavs-MacBook-Pro.local>
2023-12-10 19:36:54 -0500

    Added Delivery Request, Main.cpp, makefile, route scatter plot graph

--- DeliveryRequest.h
+++ DeliveryRequest.h
@@ -6,11 +6,16 @@
 
 class DeliveryRequest {
 public:
+    DeliveryRequest();  // Default constructor
+
     DeliveryRequest(const Address& address, bool isPrime, int deadline);
 
     const Address& getAddress() const;
     bool getIsPrime() const;
     int getDeadline() const;
+    bool operator==(const DeliveryRequest& other) const;
+        bool isValid() const;  
+
 
 private:
     Address address;
--- main.cpp
+++ main.cpp
@@ -1,82 +1,77 @@
+#include "Schedule.h"
 #include "DeliveryRequest.h"
-#include "Route.h"
 #include "Address.h"
 #include <iostream>
 #include <vector>
-
-
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 int main() {
-    // Example Addresses for Delivery Requests
-    {
-    std::cout<<"Test Case 1"<<std::endl;
-    std::vector<Address> addresses = {
-       Address(2, 3),
-        Address(4, 6),
-         Address(8, 9),
-        Address(1, 7)
-       
-    };
+    // Initialize random seed
+    std::srand(std::time(nullptr));
 
+    // Initialize the number of trucks and maximum deliveries per day
+    const int numTrucks = 3;
+    const int maxDeliveriesPerDay = 5;
 
-    // Convert addresses to delivery requests
+    // Create a list of random addresses for delivery
+   std::vector<Address> addresses;
+    //    std::vector<Address> addresses = {
+    //    Address(2, 3),
+    //     Address(4, 6),
+    //      Address(8, 9),
+    //     Address(1, 7),
+    //     Address(2, 3),
+    //     Address(3,9),
+    //      Address(3,0),    
+    //      Address(4, 6),
+    //      Address(8, 9),
+    //     Address(1, 7),
+    //     Address(2, 3),
+    //     Address(3,9),
+    //      Address(3,0)   
+    // };
+    const int numAddresses = 50; // Number of addresses
+    for (int i = 0; i < numAddresses; ++i) {
+        int x = std::rand() % 10+1; // Random x coordinate (0 to 9)
+        int y = std::rand() % 10+1; // Random y coordinate (0 to 9)
+        addresses.push_back(Address(x, y));
+    }
+int i=1;
+    // Convert addresses to delivery requests with random prime status and deadline
     std::vector<DeliveryRequest> deliveryRequests;
     for (const auto& addr : addresses) {
-        deliveryRequests.push_back(DeliveryRequest(addr, false, 3)); // Non-prime, arbitrary deadline
+       // bool isPrime = std::rand() % 2 == 0; // Randomly assign prime status
+     bool isPrime;
+       if(i<=5)
+       isPrime=1;
+       else
+       isPrime=0;
+       i++;
+
+      // cout<<isPrime<<endl;
+        int deadline = isPrime ? 1 : (std::rand() % 3 + 2); // Deadline: 1 for prime, 2-4 for non-prime
+        deliveryRequests.push_back(DeliveryRequest(addr, isPrime, deadline));
     }
 
-    // Create a Route object and use the Greedy Nearest Neighbor algorithm
-    Route route;
-    route.distance(addresses);
-   // std::cout << "Total Distance without Greedy Algorithm: " << totalDistance << " units\n";
-  
-   // std::cout << "\nTotal Distance: " << route.calculateTotalDistance() << " units\n";
-    route.createNearestNeighborRoute(deliveryRequests);
- 
+        // Print delivery requests
+    // cout << "Delivery Requests:" << endl;
+    // for (const auto& request : deliveryRequests) {
+    //     cout << "Address: (" << request.getAddress().getX() << ", " << request.getAddress().getY() << "), ";
+    //     cout << "Prime: " << (request.getIsPrime() ? "Yes" : "No") << ", ";
+    //     cout << "Deadline: " << request.getDeadline() << endl;
+    // }
 
-    // Print the constructed route
-    std::cout << "Constructed Route after greedy: ";
-    for (const auto& request : route.getDeliveryRequests()) {
-        const Address& addr = request.getAddress();
-        std::cout << "(" << addr.getX() << ", " << addr.getY() << ") ";
+    // Create and initialize the schedule
+    Schedule schedule(numTrucks, maxDeliveriesPerDay);
+    for (const auto& request : deliveryRequests) {
+        schedule.addDeliveryRequest(request);
     }
-    std::cout << "\nTotal Distance after greedy: " << route.calculateTotalDistance() << " units\n";
-    }
-
-    {
-         std::cout<<"Test Case 2"<<std::endl;
-    std::vector<Address> addresses = {
-       Address(1, 2),
-        Address(3, 4),
-       Address(9, 0),
-         Address(5, 6),
-        Address(7, 8)
-       
-    };
-
-    // Convert addresses to delivery requests
-    std::vector<DeliveryRequest> deliveryRequests;
-    for (const auto& addr : addresses) {
-        deliveryRequests.push_back(DeliveryRequest(addr, false, 3)); // Non-prime, arbitrary deadline
-    }
-
-    // Create a Route object and use the Greedy Nearest Neighbor algorithm
-    Route route;
-    route.distance(addresses);
-   // std::cout << "Total Distance without Greedy Algorithm: " << totalDistance << " units\n";
-  
-   // std::cout << "\nTotal Distance: " << route.calculateTotalDistance() << " units\n";
-    route.createNearestNeighborRoute(deliveryRequests);
- 
-
-    // Print the constructed route
-    std::cout << "Constructed Route after greedy: ";
-    for (const auto& request : route.getDeliveryRequests()) {
-        const Address& addr = request.getAddress();
-        std::cout << "(" << addr.getX() << ", " << addr.getY() << ") ";
-    }
-    std::cout << "\nTotal Distance after greedy: " << route.calculateTotalDistance() << " units\n";
+Schedule::initializeCSV("route_data.csv");
+    // Generate and display the schedule
+    schedule.createSchedule();
+    schedule.displaySchedule();
+    schedule.writeToCSV("delivery_schedule.csv");
     
-    }
     return 0;
 }
-
